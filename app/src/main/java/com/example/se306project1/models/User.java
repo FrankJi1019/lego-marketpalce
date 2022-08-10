be485e3d9dbdbb4a1b0702940fd8e79dc95dc6d1
xzhu429 <xzhu429@aucklanduni.ac.nz>
2022-08-10 21:22:27 +1200

    add IUer and User model class

---
+++ app/src/main/java/com/example/se306project1/models/User.java
@@ -0,0 +1,31 @@
+package com.example.se306project1.models;
+
+public class User implements IUser{
+    private String username;
+    private String password;
+
+    public User() {}
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+}
